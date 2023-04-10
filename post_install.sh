d87f2d044cd2daa0a1fc4f4af0cd287bff615c10
Fathul Karim <fathul.karim@gmail.com>
2023-04-10 21:05:05 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -15,4 +15,4 @@ service mysql-server start
 
 # Step 3 — Installing PHP
 pkg install php80 php80-mysqli mod_php80
-cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+# cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
