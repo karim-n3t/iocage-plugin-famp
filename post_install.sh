1fec5fd6bcd2c1c551edecda6a609e3398510bc6
Fathul Karim <fathul.karim@gmail.com>
2023-04-10 21:13:28 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,12 +6,12 @@ pw add user -n famp -c Famp -s /bin/csh -m
 # Step 1 — Installing Apache
 pkg install apache24
 sysrc apache24_enable="YES"
-service apache24 start
+# service apache24 start
 
 # Step 2 — Installing MySQL
 pkg install mysql80-server
 sysrc mysql_enable="YES"
-service mysql-server start
+# service mysql-server start
 
 # Step 3 — Installing PHP
 pkg install php80 php80-mysqli mod_php80
