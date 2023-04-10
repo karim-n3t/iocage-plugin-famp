b029c3a39a8043c22dfedb70dcab18c49d300948
Fathul Karim <fathul.karim@gmail.com>
2023-04-10 21:33:00 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Create user 'famp'
-pw add user -n famp -c Famp -s /bin/csh -m
+echo "Enable and start Apache"
+sysrc apache24_enable=YES
+service apache24 start
 
-# Step 1 — Installing Apache
-pkg install apache24
-# sysrc apache24_enable="YES"
-# service apache24 start
+echo "Enable and start mysql server"
+sysrc mysql_enable=YES
+service mysql-server start
 
-# Step 2 — Installing MySQL
-pkg install mysql80-server
-# sysrc mysql_enable="YES"
-# service mysql-server start
-
-# Step 3 — Installing PHP
-pkg install php80 php80-mysqli mod_php80
-# cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+echo "Create php.ini"
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
